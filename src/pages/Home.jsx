f790a2798aa3a21c02033e3d956c14dbb1b55258
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-11-11 21:13:57 -0700

    Add HomePage Content and Style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <h1> Welcome to Team Dex!</h1>
+      <h3> Build your dream squad of Pokémon </h3>
+      <p>
+        Design your own creatures, customize their stats, and assemble
+        unbeatable teams.
+        <span>Your Pokédex, your rules. 🧢🔥</span>
+        {/* TODO: Will Add some pictures or animation here later */}
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
