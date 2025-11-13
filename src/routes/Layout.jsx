5554dd6e09581b88cfc108351d6c109b7c2dd909
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-11-13 00:16:04 -0700

    Add rolling Pokeball to Layout

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,10 +1,15 @@
 import NavBar from '../components/NavBar'
 import { Outlet } from 'react-router'
+import pokeballIcon from '../assets/pokeball.svg'
+import './Layout.css'
 
 const Layout = () => {
   return (
     <div className='layout'>
       <NavBar />
+      <div className="rolling-pokeball">
+        <img src={pokeballIcon} alt="Rolling Pokéball" className="pokeball-animation" />
+      </div>
       <Outlet />
     </div>
   )
